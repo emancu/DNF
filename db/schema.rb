ba30d01dc362d7a2b317ed6deb52d9a788bed7a0
eMancu <emiliano.mancuso@gmail.com>
2011-06-08 16:43:41 -0300

    Fix description problems

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091107041328) do
+ActiveRecord::Schema.define(:version => 20110608042928) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "title",            :limit => 50, :default => ""
+    t.text     "comment",                        :default => ""
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
+  add_index "comments", ["commentable_type"], :name => "index_comments_on_commentable_type"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "lists", :force => true do |t|
     t.string   "name"
@@ -31,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20091107041328) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "completed"
+    t.string   "description"
   end
 
   create_table "users", :force => true do |t|
